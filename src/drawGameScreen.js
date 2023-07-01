608f38ccd7727b70eae41a82d047c2526a7bd12a
jakebrowning90 <jacobbrowning90@gmail.com>
2023-07-01 19:42:57 -0400

    Rewrite poem as unsorted list

--- src/drawGameScreen.js
+++ src/drawGameScreen.js
@@ -12,7 +12,45 @@ const drawGameScreen = () => {
 
     const gameInstructionDiv = document.createElement('div');
     gameInstructionDiv.setAttribute("id", "gameInstructionDiv");
-    gameInstructionDiv.textContent = "I spy a clock, a birdhouse, a duck, five thimbles, a plane, two fish in a truck; A big red apple, a zebra jeep, STOP, GO, and BEEP BEEP BEEP. Click an area of the picture to call the option list."
+    gameInstructionDiv.textContent = "I spy..."
+
+    //TODO - reduce poem to function
+    const itemPoem = document.createElement('ul');
+    itemPoem.setAttribute("id", "itemPoem");
+    const itemClock = document.createElement('li');
+    itemClock.textContent = "a clock,"
+    const itemBirdhouse = document.createElement('li');
+    itemBirdhouse.textContent = "a birdhouse,"
+    const itemDuck = document.createElement('li');
+    itemDuck.textContent = "a duck,"
+    const itemThimbles = document.createElement('li');
+    itemThimbles.textContent = "five thimbles,"
+    const itemPlane = document.createElement('li');
+    itemPlane.textContent = "a plane,"
+    const itemFish = document.createElement('li');
+    itemFish.textContent = "two fish in a truck;"
+    const itemApple = document.createElement('li');
+    itemApple.textContent = "A big red apple,"
+    const itemJeep = document.createElement('li');
+    itemJeep.textContent = "a zebra jeep,"
+    const itemStop = document.createElement('li');
+    itemStop.textContent = "STOP,"
+    const itemGo = document.createElement('li');
+    itemGo.textContent = "GO,"
+    const itemBeep= document.createElement('li');
+    itemBeep.textContent = "and BEEP BEEP BEEP."
+
+    itemPoem.appendChild(itemClock)
+    itemPoem.appendChild(itemBirdhouse)
+    itemPoem.appendChild(itemDuck)
+    itemPoem.appendChild(itemThimbles)
+    itemPoem.appendChild(itemPlane)
+    itemPoem.appendChild(itemFish)
+    itemPoem.appendChild(itemApple)
+    itemPoem.appendChild(itemJeep)
+    itemPoem.appendChild(itemStop)
+    itemPoem.appendChild(itemGo)
+    itemPoem.appendChild(itemBeep)
 
     const characterPickerSpace = document.createElement('div');
     characterPickerSpace.setAttribute("id", "characterPickerSpace");
@@ -35,6 +73,7 @@ const drawGameScreen = () => {
     // gameScreenContent.appendChild(homeScreenButton);
     gameScreenContent.appendChild(gameControlDiv);
     gameControlDiv.appendChild(gameInstructionDiv)
+    gameInstructionDiv.appendChild(itemPoem)
     gameControlDiv.appendChild(characterPickerSpace)
     gameControlDiv.appendChild(homeScreenButton)
     gameScreenContent.appendChild(gameScreenDiv);
